0e1c4873dc82933f1437220ad15d2c82b75704f0
menthe <adrien.le.mentheour@gmail.com>
2025-03-05 15:51:21 +0100

    🔧🌐  ui

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: 'English',
+    fr: 'French',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'case.challenges': 'Strategic challenges:',
+      'case.response': 'Response:',
+    },
+    fr: {
+      'case.challenges': 'Enjeux strat’ :',
+      'case.response': 'Réponse :',
+    },
+  } as const;
