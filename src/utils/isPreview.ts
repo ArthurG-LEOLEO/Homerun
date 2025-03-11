2ba1dbca8969fa4191392f69932ff1691fa1011f
Clément Lemoine <clementlemoine.cl@gmail.com>
2025-03-11 11:35:57 +0100

    🚧 debug netlify ssr

--- src/utils/isPreview.ts
+++ src/utils/isPreview.ts
@@ -1,3 +1,9 @@
 export default function isPreview() {
+    console.log(
+        "env var: ",
+        import.meta.env,
+        import.meta.env.STORYBLOK_PREVIEW,
+    );
+
     return import.meta.env.STORYBLOK_PREVIEW === "true";
 }
